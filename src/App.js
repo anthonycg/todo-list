cd8be6e3ef80f916b68eca454b97ab5d62b0999d
Anthony <anthonygibson24@gmail.com>
2022-05-20 11:18:35 -0500

    strikethrough works on check

--- src/App.js
+++ src/App.js
@@ -4,14 +4,37 @@ import { useState } from 'react';
 import TodoForm from "./components/TodoForm"
 
 function App() { 
-  const [checked, setChecked] = useState(false);
+  // const [checked, setChecked] = useState(false);
+    const [todoItem, setTodoItem] = useState("");
+    const [todoArray, setTodoArray] = useState([]);
+    const [checked, setChecked] = useState(false);
+    const handleTodoItem = (e) => {
+        e.preventDefault();
+        setTodoItem(todoItem);
+        console.log(todoItem);
+        // create an object that contains inputted value
+        const newTodoObj = {
+            todo: todoItem,
+            completed: false
+        }
+        console.log(todoArray);
+        setTodoArray([...todoArray, newTodoObj]);
+        // create array to add new todo items to
+        const todoList = [...todoArray, newTodoObj];
+        console.log(todoList)
+        console.log("completed status" + newTodoObj.completed);
+        setTodoItem("");
+        
+    }
   return (
     <div className="App">
       <header className="back-splash">
-        <TodoForm />
+        
+        <TodoForm todoItem={todoItem} setTodoItem={setTodoItem} todoArray={todoArray} setTodoArray={setTodoArray} checked={checked} setChecked={setChecked} newTodoObj handleTodoItem={handleTodoItem}/>
       </header>
     </div>
   );
 }
+
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,41 +1,42 @@
 import React from 'react'
 import { useState } from 'react'
-
 const TodoForm = (props) => {
-    const [todoItem, setTodoItem] = useState("");
-    const [todoArray, setTodoArray] = useState([]);
-    const [checked, setChecked] = useState(false);
-    const handleTodoItem = (e) => {
-        e.preventDefault();
-        setTodoItem(todoItem);
-        console.log(todoItem);
-        // create an object that contains inputted value
-        const newTodoObj = {
-            todo: todoItem,
-            completed: checked
-        }
-        console.log(todoArray);
-        setTodoArray([...todoArray, newTodoObj]);
-        // create array to add new todo items to
-        const todoList = [...todoArray, newTodoObj];
-        console.log(todoList)
-        console.log("completed status" + newTodoObj.completed);
+    const {newTodoObj, todoArray, setTodoArray, setChecked, checked, setTodoItem, todoItem, handleTodoItem} = props;
+// const TodoForm = (props) => {
+//     const [todoItem, setTodoItem] = useState("");
+//     const [todoArray, setTodoArray] = useState([]);
+//     const [checked, setChecked] = useState(false);
+//     const handleTodoItem = (e) => {
+//         e.preventDefault();
+//         setTodoItem(todoItem);
+//         console.log(todoItem);
+//         // create an object that contains inputted value
+//         const newTodoObj = {
+//             todo: todoItem,
+//             completed: false
+//         }
+//         console.log(todoArray);
+//         setTodoArray([...todoArray, newTodoObj]);
+//         // create array to add new todo items to
+//         const todoList = [...todoArray, newTodoObj];
+//         console.log(todoList)
+//         console.log("completed status" + newTodoObj.completed);
+//         setTodoItem("");
         
-        
-    }
+//     }
 
-    const handleCheckmark = (e) => {
-        setChecked(!checked);
-        console.log(checked);
-        return checked ? console.log("it's true"): todoArray.filter( (item, index) => {index.remove()} )
+    const handleCheckmark = () => {
+        setChecked(current => !current);
+        // console.log(checked);
+        // return checked ? console.log("it's true"): todoArray.filter( (item, index) => {index.remove()} )
     }
 
     const handleRemove = (index) => {
-        const editedArray = todoArray.filter( (item, index) => item.index === index);
+        const editedArray = todoArray.filter( (item, index) => index !== 1);
         setTodoArray(editedArray);
         console.log(editedArray);
     }
-
+    
     return (
         <div>
         <form onSubmit={ handleTodoItem }>
@@ -50,13 +51,14 @@ const TodoForm = (props) => {
                 <div>{todoArray.map( (items, index) => {
                     return (
                         <div key={index}> 
-                        {/* <input type="checkbox" value={checked} onChange={handleCheckmark}></input> */}
-                        <p>Todo: {items.todo} <button onClick={handleRemove}>Done</button></p>
+                        <p style= {{textDecorationLine: checked ? 'line-through': '', textDecorationStyle: checked ? 'solid' : ''}}>{items.todo}<input type="checkbox" checked={checked} onChange={handleCheckmark}></input></p>
+                        {/* <button onClick={handleRemove}>Completed</button> */}
                         </div>
                         )} )}</div>
             </div>
         </div>
     )
 }
+
 
 export default TodoForm;
